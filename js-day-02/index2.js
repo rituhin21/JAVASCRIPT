43a312b5cd6221722d2f4dc5243f5217b470fe94
RI Tuhin <contact.rijuan@gmail.com>
2024-02-27 12:59:34 +0600

    second commit

--- js-day-02/index2.js
+++ js-day-02/index2.js
@@ -1,1 +1,22 @@
 console.log("Expressions & Operators");
+
+// Interview Questions
+
+// Question: 01
+// var result = "hello" / 2;
+// console.log(result);
+// if string divide by number the output will be NaN 
+
+// Question: 02
+// var result = 0.1 + 0.2;
+// console.log(result.toFixed(3));
+// Output: 0.30000000000000004
+// the output will be so large to fixed the problem the toFixed(values which we want to show after the radix point) method will be applied here  
+
+// Question: 03 
+// var result = 55 * "Hello";
+// console.log(result);
+// "String * Number or String / Number" the output will be NaN 
+
+// Question: 04
+console.log(5 === "5")
